35ef2356eaffb7dc68eae1cf5bfeeaac3ae13a36
Muh. Afdal Maulana Said <afdalmaulanaaa@gmail.com>
2023-11-30 22:30:23 +0800

    ongoing table

--- my-test/src/components/TableList.jsx
+++ my-test/src/components/TableList.jsx
@@ -1,9 +1,186 @@
 import React, { useState } from "react";
+import { HiMiniComputerDesktop } from "react-icons/hi2";
 import { Table } from "antd";
 const columns = [
   {
     title: "ID",
     dataIndex: "ID",
+    render: (_, record) => (
+      <>
+        <div>User ID </div>
+        <div style={{ fontWeight: "bold" }}>457 </div>
+        <div>Employee ID</div>
+        <div style={{ fontWeight: "bold" }}>BAS-51243</div>
+      </>
+    ),
+    filters: [
+      {
+        text: "Joe",
+        value: "Joe",
+      },
+      {
+        text: "Category 1",
+        value: "Category 1",
+        children: [
+          {
+            text: "Yellow",
+            value: "Yellow",
+          },
+          {
+            text: "Pink",
+            value: "Pink",
+          },
+        ],
+      },
+      {
+        text: "Category 2",
+        value: "Category 2",
+        children: [
+          {
+            text: "Green",
+            value: "Green",
+          },
+          {
+            text: "Black",
+            value: "Black",
+          },
+        ],
+      },
+    ],
+    filterMode: "tree",
+    filterSearch: true,
+    onFilter: (value, record) => record.name.includes(value),
+    width: "20%",
+  },
+  {
+    title: "Status",
+    dataIndex: "Status",
+    filters: [
+      {
+        text: "Joe",
+        value: "Joe",
+      },
+      {
+        text: "Category 1",
+        value: "Category 1",
+        children: [
+          {
+            text: "Yellow",
+            value: "Yellow",
+          },
+          {
+            text: "Pink",
+            value: "Pink",
+          },
+        ],
+      },
+      {
+        text: "Category 2",
+        value: "Category 2",
+        children: [
+          {
+            text: "Green",
+            value: "Green",
+          },
+          {
+            text: "Black",
+            value: "Black",
+          },
+        ],
+      },
+    ],
+    filterMode: "tree",
+    filterSearch: true,
+    onFilter: (value, record) => record.name.includes(value),
+    width: "20%",
+  },
+  {
+    title: "Name",
+    dataIndex: "Name",
+    filters: [
+      {
+        text: "Joe",
+        value: "Joe",
+      },
+      {
+        text: "Category 1",
+        value: "Category 1",
+        children: [
+          {
+            text: "Yellow",
+            value: "Yellow",
+          },
+          {
+            text: "Pink",
+            value: "Pink",
+          },
+        ],
+      },
+      {
+        text: "Category 2",
+        value: "Category 2",
+        children: [
+          {
+            text: "Green",
+            value: "Green",
+          },
+          {
+            text: "Black",
+            value: "Black",
+          },
+        ],
+      },
+    ],
+    filterMode: "tree",
+    filterSearch: true,
+    onFilter: (value, record) => record.name.includes(value),
+    width: "20%",
+  },
+  {
+    title: "Contact",
+    dataIndex: "Contact",
+    filters: [
+      {
+        text: "Joe",
+        value: "Joe",
+      },
+      {
+        text: "Category 1",
+        value: "Category 1",
+        children: [
+          {
+            text: "Yellow",
+            value: "Yellow",
+          },
+          {
+            text: "Pink",
+            value: "Pink",
+          },
+        ],
+      },
+      {
+        text: "Category 2",
+        value: "Category 2",
+        children: [
+          {
+            text: "Green",
+            value: "Green",
+          },
+          {
+            text: "Black",
+            value: "Black",
+          },
+        ],
+      },
+    ],
+    filterMode: "tree",
+    filterSearch: true,
+    onFilter: (value, record) => record.name.includes(value),
+    width: "20%",
+  },
+  {
+    title: "Address",
+    dataIndex: "Address",
     filters: [
       {
         text: "Joe",
@@ -44,23 +221,45 @@ const columns = [
     width: "30%",
   },
   {
-    title: "Age",
-    dataIndex: "age",
+    title: "Trip",
+    dataIndex: "Trip",
+  },
+  {
+    title: "Voucher",
+    dataIndex: "Voucher",
+  },
+  {
+    title: "Action",
+    dataIndex: "Action",
   },
   {
     title: "Address",
     dataIndex: "address",
   },
 ];
-const data = [];
-for (let i = 0; i < 46; i++) {
-  data.push({
-    key: i,
-    name: `Edward King ${i}`,
-    age: 32,
-    address: `London, Park Lane no. ${i}`,
-  });
-}
+const data = [
+  {
+    key: 1,
+    ID: "User ID",
+    Status: 32,
+    Name: "afdal",
+  },
+  {
+    key: 2,
+    ID: "User ID",
+    Status: 32,
+    Name: "afdal",
+  },
+];
+// for (let i = 0; i < 46; i++) {
+//   data.push({
+//     key: i,
+//     ID: [`User ID`, "457", "Employee ID"],
+//     Status: 32,
+//     Name: "afdal",
+//     address: `London, Park Lane no. ${i}`,
+//   });
+// }
 const TableList = () => {
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
   const onSelectChange = (newSelectedRowKeys) => {
